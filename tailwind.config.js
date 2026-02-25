847ab48e9265dcf83eecefdb7c802b34339d5c87
douliacameroun <douliacameroun@gmail.com>
2026-02-25 12:18:43 +0000

    finaly

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        gold: '#D4AF37',
-        darkgreen: '#013220',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
